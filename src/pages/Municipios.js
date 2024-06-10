5ddad9b2cfea8c2f0009ee0c87e62a5539356416
Paulo-Santos20 <paulo_santos20@outlook.com.br>
2024-06-10 16:52:21 -0300

    fix: issues graficos

--- src/pages/Municipios.js
+++ src/pages/Municipios.js
@@ -8,27 +8,28 @@ import taboao from "../images/prefeiturafeira.png";
 
 const Municipios = () => {
   return (
-    <div className="municipios-page">      
+    <div className="municipios-page">
       <div className='Banner'>
-      <Link to="/coracao-de-maria" className="municipio-link">
-        <img src={coracao} alt="Coração de Maria" className="municipio-image" />
-      </Link>
-      <Link to="/conceicao-da-feira" className="municipio-link">
-        <img src={conceicao} alt="Conceição da Feira" className="municipio-image" />
-      </Link>
-      <Link to="/taboao-da-serra" className="municipio-link">
-        <img src={taboao} alt="Taboão da Serra" className="municipio-image" />
-      </Link>
-      <Link to="/feira-de-santana" className="municipio-link">
-        <img src={feira} alt="Feira de Santana" className="municipio-image" />
-      </Link>
+        <Link to="/coracao-de-maria" className="municipio-link">
+          <img src={coracao} alt="Coração de Maria" className="municipio-image" />
+        </Link>
+        <Link to="/conceicao-da-feira" className="municipio-link">
+          <img src={conceicao} alt="Conceição da Feira" className="municipio-image" />
+        </Link>
+        <Link to="/taboao-da-serra" className="municipio-link">
+          <img src={taboao} alt="Taboão da Serra" className="municipio-image" />
+        </Link>
+        <Link to="/feira-de-santana" className="municipio-link">
+          <img src={feira} alt="Feira de Santana" className="municipio-image" />
+        </Link>
       </div>
-      <div className='grafico'>
-      <iframe width="40%" height="350" class="chart-iframe" seamless frameborder="0" scrolling="no" src="https://docs.google.com/spreadsheets/d/e/2PACX-1vSFnRNk-krJz-U0AMu2IvgxAic6jcQEWh6LtM_rhGyAFzw_mi4niFMHyl9MYB2HbFkRL2mdftTicVAg/pubchart?oid=1063323562&amp;format=interactive"></iframe>
-      <iframe width="40%" height="350" seamless frameborder="0" scrolling="no" src="https://docs.google.com/spreadsheets/d/e/2PACX-1vSFnRNk-krJz-U0AMu2IvgxAic6jcQEWh6LtM_rhGyAFzw_mi4niFMHyl9MYB2HbFkRL2mdftTicVAg/pubchart?oid=1504166093&amp;format=interactive"></iframe>
-      <iframe width="40%" height="315" seamless frameborder="0" scrolling="no" src="https://docs.google.com/spreadsheets/d/e/2PACX-1vSFnRNk-krJz-U0AMu2IvgxAic6jcQEWh6LtM_rhGyAFzw_mi4niFMHyl9MYB2HbFkRL2mdftTicVAg/pubchart?oid=1063323562&amp;format=interactive"></iframe>
-
-      </div>
+      <div class="grafico">
+  <div class="iframe-container">
+    <iframe class="iframe-grande" src="https://docs.google.com/spreadsheets/d/e/2PACX-1vSFnRNk-krJz-U0AMu2IvgxAic6jcQEWh6LtM_rhGyAFzw_mi4niFMHyl9MYB2HbFkRL2mdftTicVAg/pubchart?oid=1063323562&amp;format=interactive"></iframe>
+    <iframe class="iframe-pequeno" src="https://docs.google.com/spreadsheets/d/e/2PACX-1vSFnRNk-krJz-U0AMu2IvgxAic6jcQEWh6LtM_rhGyAFzw_mi4niFMHyl9MYB2HbFkRL2mdftTicVAg/pubchart?oid=1504166093&amp;format=interactive"></iframe>
+    <iframe class="iframe-pequeno" src="https://docs.google.com/spreadsheets/d/e/2PACX-1vSFnRNk-krJz-U0AMu2IvgxAic6jcQEWh6LtM_rhGyAFzw_mi4niFMHyl9MYB2HbFkRL2mdftTicVAg/pubchart?oid=1063323562&amp;format=interactive"></iframe>
+  </div>
+</div>
     </div>
   );
 }
