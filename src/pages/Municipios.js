01222029927a3f6df9f146b7792e39456f2de82c
Paulo-Santos20 <paulo_santos20@outlook.com.br>
2024-06-11 12:35:11 -0300

    fix smartphone issues

--- src/pages/Municipios.js
+++ src/pages/Municipios.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useEffect } from 'react';
+import { Link } from 'react-router-dom'; // Importando Link do react-router-dom
 import "./style.css";
 import coracao from "../images/coracaodemaria.png";
 import conceicao from "../images/conceicaodafeira.png";
@@ -7,6 +7,29 @@ import feira from "../images/taboaodaserra.png";
 import taboao from "../images/prefeiturafeira.png";
 
 const Municipios = () => {
+  useEffect(() => {
+    const adjustIframeSrc = () => {
+      const iframes = document.querySelectorAll('.iframe-grande, .iframe-pequeno');
+      if (window.innerWidth <= 768) {
+        iframes.forEach((iframe) => {
+          const mobileSrc = iframe.getAttribute('data-mobile-src');
+          if (mobileSrc) {
+            iframe.setAttribute('src', mobileSrc);
+          }
+        });
+      }
+    };
+
+    window.addEventListener('resize', adjustIframeSrc);
+    window.addEventListener('load', adjustIframeSrc);
+
+    // Cleanup event listeners on component unmount
+    return () => {
+      window.removeEventListener('resize', adjustIframeSrc);
+      window.removeEventListener('load', adjustIframeSrc);
+    };
+  }, []);
+
   return (
     <div className="municipios-page">
       <div className='Banner'>
@@ -23,15 +46,18 @@ const Municipios = () => {
           <img src={feira} alt="Feira de Santana" className="municipio-image" />
         </Link>
       </div>
-      <div class="grafico">
-  <div class="iframe-container">
-    <iframe class="iframe-grande" src="https://docs.google.com/spreadsheets/d/e/2PACX-1vSFnRNk-krJz-U0AMu2IvgxAic6jcQEWh6LtM_rhGyAFzw_mi4niFMHyl9MYB2HbFkRL2mdftTicVAg/pubchart?oid=1063323562&amp;format=interactive"></iframe>
-    <iframe class="iframe-pequeno" src="https://docs.google.com/spreadsheets/d/e/2PACX-1vSFnRNk-krJz-U0AMu2IvgxAic6jcQEWh6LtM_rhGyAFzw_mi4niFMHyl9MYB2HbFkRL2mdftTicVAg/pubchart?oid=1504166093&amp;format=interactive"></iframe>
-    <iframe class="iframe-pequeno" src="https://docs.google.com/spreadsheets/d/e/2PACX-1vSFnRNk-krJz-U0AMu2IvgxAic6jcQEWh6LtM_rhGyAFzw_mi4niFMHyl9MYB2HbFkRL2mdftTicVAg/pubchart?oid=1063323562&amp;format=interactive"></iframe>
-  </div>
-</div>
+      <div className="grafico">
+        <div className="iframe-container">
+          <iframe className="iframe-grande" src="https://docs.google.com/spreadsheets/d/e/2PACX-1vSFnRNk-krJz-U0AMu2IvgxAic6jcQEWh6LtM_rhGyAFzw_mi4niFMHyl9MYB2HbFkRL2mdftTicVAg/pubchart?oid=1063323562&amp;format=interactive"
+            data-mobile-src="https://docs.google.com/spreadsheets/d/e/2PACX-1vSFnRNk-krJz-U0AMu2IvgxAic6jcQEWh6LtM_rhGyAFzw_mi4niFMHyl9MYB2HbFkRL2mdftTicVAg/pubchart?oid=675843512&amp;format=interactive"></iframe>
+          <iframe className="iframe-pequeno" src="https://docs.google.com/spreadsheets/d/e/2PACX-1vSFnRNk-krJz-U0AMu2IvgxAic6jcQEWh6LtM_rhGyAFzw_mi4niFMHyl9MYB2HbFkRL2mdftTicVAg/pubchart?oid=1504166093&amp;format=interactive"
+            data-mobile-src="https://docs.google.com/spreadsheets/d/e/2PACX-1vSFnRNk-krJz-U0AMu2IvgxAic6jcQEWh6LtM_rhGyAFzw_mi4niFMHyl9MYB2HbFkRL2mdftTicVAg/pubchart?oid=1886583363&amp;format=interactive"></iframe>
+          <iframe className="iframe-pequeno" src="https://docs.google.com/spreadsheets/d/e/2PACX-1vSFnRNk-krJz-U0AMu2IvgxAic6jcQEWh6LtM_rhGyAFzw_mi4niFMHyl9MYB2HbFkRL2mdftTicVAg/pubchart?oid=1063323562&amp;format=interactive"
+            data-mobile-src="https://docs.google.com/spreadsheets/d/e/2PACX-1vSFnRNk-krJz-U0AMu2IvgxAic6jcQEWh6LtM_rhGyAFzw_mi4niFMHyl9MYB2HbFkRL2mdftTicVAg/pubchart?oid=675843512&amp;format=interactive"></iframe>
+        </div>
+      </div>
     </div>
   );
-}
+};
 
 export default Municipios;
