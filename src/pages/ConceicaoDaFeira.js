4f51e2e8c11ea8439c765d837a4942e58242b710
Paulo-Santos20 <paulo_santos20@outlook.com.br>
2024-06-11 14:21:27 -0300

    feat: conceicaodafeira page

--- src/pages/ConceicaoDaFeira.js
+++ src/pages/ConceicaoDaFeira.js
@@ -1,12 +1,41 @@
-// Municipios.js
-import React from 'react';
+import React, { useEffect } from 'react';
+import "./style.css";
 
 const Municipios = () => {
-  return (
-    <div>
-      <h1>Conceiçlão da Feira</h1>
-      <p>Conteúdo da página.</p>
-    </div>
+  useEffect(() => {
+    const adjustIframeSrc = () => {
+      const iframes = document.querySelectorAll('.iframe-grande, .iframe-pequeno');
+      if (window.innerWidth <= 768) {
+        iframes.forEach((iframe) => {
+          const mobileSrc = iframe.getAttribute('data-mobile-src');
+          if (mobileSrc) {
+            iframe.setAttribute('src', mobileSrc);
+          }
+        });
+      }
+    };
+
+    window.addEventListener('resize', adjustIframeSrc);
+    window.addEventListener('load', adjustIframeSrc);
+
+    // Cleanup event listeners on component unmount
+    return () => {
+      window.removeEventListener('resize', adjustIframeSrc);
+      window.removeEventListener('load', adjustIframeSrc);
+    };
+  }, []);
+
+  return (   
+      <div className="grafico">
+        <div className="iframe-container">
+          <iframe className="iframe-grande" src="https://docs.google.com/spreadsheets/d/e/2PACX-1vSFnRNk-krJz-U0AMu2IvgxAic6jcQEWh6LtM_rhGyAFzw_mi4niFMHyl9MYB2HbFkRL2mdftTicVAg/pubchart?oid=1063323562&amp;format=interactive"
+            data-mobile-src="https://docs.google.com/spreadsheets/d/e/2PACX-1vSFnRNk-krJz-U0AMu2IvgxAic6jcQEWh6LtM_rhGyAFzw_mi4niFMHyl9MYB2HbFkRL2mdftTicVAg/pubchart?oid=675843512&amp;format=interactive"></iframe>
+          <iframe className="iframe-pequeno" src="https://docs.google.com/spreadsheets/d/e/2PACX-1vSFnRNk-krJz-U0AMu2IvgxAic6jcQEWh6LtM_rhGyAFzw_mi4niFMHyl9MYB2HbFkRL2mdftTicVAg/pubchart?oid=1504166093&amp;format=interactive"
+            data-mobile-src="https://docs.google.com/spreadsheets/d/e/2PACX-1vSFnRNk-krJz-U0AMu2IvgxAic6jcQEWh6LtM_rhGyAFzw_mi4niFMHyl9MYB2HbFkRL2mdftTicVAg/pubchart?oid=1886583363&amp;format=interactive"></iframe>
+          <iframe className="iframe-pequeno" src="https://docs.google.com/spreadsheets/d/e/2PACX-1vSFnRNk-krJz-U0AMu2IvgxAic6jcQEWh6LtM_rhGyAFzw_mi4niFMHyl9MYB2HbFkRL2mdftTicVAg/pubchart?oid=1063323562&amp;format=interactive"
+            data-mobile-src="https://docs.google.com/spreadsheets/d/e/2PACX-1vSFnRNk-krJz-U0AMu2IvgxAic6jcQEWh6LtM_rhGyAFzw_mi4niFMHyl9MYB2HbFkRL2mdftTicVAg/pubchart?oid=675843512&amp;format=interactive"></iframe>
+        </div>
+      </div>    
   );
 };
 
