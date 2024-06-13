f084216c46b7fbd87e87ce81771a463101756c77
ester <eacm@cesar.school>
2024-06-13 10:24:08 -0300

    feat: feira de santana

--- src/pages/FeiraDeSantana.js
+++ src/pages/FeiraDeSantana.js
@@ -1,11 +1,56 @@
-// Municipios.js
-import React from 'react';
+import React, { useEffect } from 'react';
+import { Link } from 'react-router-dom'; // Importando Link do react-router-dom
+import "./style.css";
 
 const Municipios = () => {
+  useEffect(() => {
+    const adjustIframeSrc = () => {
+      const iframes = document.querySelectorAll('.iframe-grande, .iframe-pequeno');
+      if (window.innerWidth <= 768) {
+        iframes.forEach((iframe) => {
+          const mobileSrc = iframe.getAttribute('data-mobile-src');
+          if (mobileSrc) {
+            iframe.setAttribute('src', mobileSrc);
+          }
+        });
+      }
+    };
+
+    window.addEventListener('resize', adjustIframeSrc);
+    window.addEventListener('load', adjustIframeSrc);
+
+    // Cleanup event listeners on component unmount
+    return () => {
+      window.removeEventListener('resize', adjustIframeSrc);
+      window.removeEventListener('load', adjustIframeSrc);
+    };
+  }, []);
   return (
-    <div>
-      <h1>FeiraDeSantana</h1>
-      <p>Conteúdo da página.</p>
+    <div className="grafico">
+      <div className="iframe-container">
+      <iframe className="iframe-pequeno" src="https://docs.google.com/spreadsheets/d/e/2PACX-1vSFnRNk-krJz-U0AMu2IvgxAic6jcQEWh6LtM_rhGyAFzw_mi4niFMHyl9MYB2HbFkRL2mdftTicVAg/pubchart?oid=1873823742&amp;format=interactive"
+          data-mobile-src=""></iframe>
+      <iframe className="iframe-pequeno" src="https://docs.google.com/spreadsheets/d/e/2PACX-1vSFnRNk-krJz-U0AMu2IvgxAic6jcQEWh6LtM_rhGyAFzw_mi4niFMHyl9MYB2HbFkRL2mdftTicVAg/pubchart?oid=1570731380&amp;format=interactive"
+          data-mobile-src=""></iframe>
+      <iframe className="iframe-pequeno" src="https://docs.google.com/spreadsheets/d/e/2PACX-1vSFnRNk-krJz-U0AMu2IvgxAic6jcQEWh6LtM_rhGyAFzw_mi4niFMHyl9MYB2HbFkRL2mdftTicVAg/pubchart?oid=1278976191&amp;format=interactive"
+          data-mobile-src=""></iframe>
+      <iframe className="iframe-pequeno" src="https://docs.google.com/spreadsheets/d/e/2PACX-1vSFnRNk-krJz-U0AMu2IvgxAic6jcQEWh6LtM_rhGyAFzw_mi4niFMHyl9MYB2HbFkRL2mdftTicVAg/pubchart?oid=1951444820&amp;format=interactive"
+          data-mobile-src=""></iframe>
+      <iframe className="iframe-pequeno" src="https://docs.google.com/spreadsheets/d/e/2PACX-1vSFnRNk-krJz-U0AMu2IvgxAic6jcQEWh6LtM_rhGyAFzw_mi4niFMHyl9MYB2HbFkRL2mdftTicVAg/pubchart?oid=1794025572&amp;format=interactive"
+          data-mobile-src=""></iframe>
+      <iframe className="iframe-pequeno" src="https://docs.google.com/spreadsheets/d/e/2PACX-1vSFnRNk-krJz-U0AMu2IvgxAic6jcQEWh6LtM_rhGyAFzw_mi4niFMHyl9MYB2HbFkRL2mdftTicVAg/pubchart?oid=1391297528&amp;format=interactive"
+          data-mobile-src=""></iframe>
+      <iframe className="iframe-pequeno" src="https://docs.google.com/spreadsheets/d/e/2PACX-1vSFnRNk-krJz-U0AMu2IvgxAic6jcQEWh6LtM_rhGyAFzw_mi4niFMHyl9MYB2HbFkRL2mdftTicVAg/pubchart?oid=1982078972&amp;format=interactive"
+          data-mobile-src=""></iframe>
+      <iframe className="iframe-pequeno" src="https://docs.google.com/spreadsheets/d/e/2PACX-1vSFnRNk-krJz-U0AMu2IvgxAic6jcQEWh6LtM_rhGyAFzw_mi4niFMHyl9MYB2HbFkRL2mdftTicVAg/pubchart?oid=1127115521&amp;format=interactive"
+          data-mobile-src=""></iframe>
+      <iframe className="iframe-pequeno" src="https://docs.google.com/spreadsheets/d/e/2PACX-1vSFnRNk-krJz-U0AMu2IvgxAic6jcQEWh6LtM_rhGyAFzw_mi4niFMHyl9MYB2HbFkRL2mdftTicVAg/pubchart?oid=1337004162&amp;format=interactive"
+          data-mobile-src=""></iframe>
+      <iframe className="iframe-pequeno" src="https://docs.google.com/spreadsheets/d/e/2PACX-1vSFnRNk-krJz-U0AMu2IvgxAic6jcQEWh6LtM_rhGyAFzw_mi4niFMHyl9MYB2HbFkRL2mdftTicVAg/pubchart?oid=1135630044&amp;format=interactive"
+          data-mobile-src=""></iframe>
+      <iframe className="iframe-pequeno" src="https://docs.google.com/spreadsheets/d/e/2PACX-1vSFnRNk-krJz-U0AMu2IvgxAic6jcQEWh6LtM_rhGyAFzw_mi4niFMHyl9MYB2HbFkRL2mdftTicVAg/pubchart?oid=864090454&amp;format=interactive"
+          data-mobile-src=""></iframe>
+      </div>
     </div>
   );
 };
