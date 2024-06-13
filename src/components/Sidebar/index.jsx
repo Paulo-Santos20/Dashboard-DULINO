83d86239b3b58008e84a861af66fbec4f2472ee6
Paulo-Santos20 <paulo_santos20@outlook.com.br>
2024-06-13 16:23:19 -0300

    style: menu icon

--- src/components/Sidebar/index.jsx
+++ src/components/Sidebar/index.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { Container, Content } from './styles';
-import { FaTimes, FaHome, FaEnvelope, FaUserAlt, FaIdCardAlt, FaRegCalendarAlt, FaChartBar } from 'react-icons/fa';
+import { FaTimes, FaHome, FaFlag, FaCross, FaCalendarDay, FaHeart, FaChessRook } from 'react-icons/fa';
 import SidebarItem from '../SidebarItem';
 import avatarDulino from "../images/avatarDulino.webp";
 
@@ -25,22 +25,22 @@ const Sidebar = ({ active, setActive }) => {
           <img src={avatarDulino} alt="Avatar Dulino" className="avatar" />
         </div>
         <Link to="/municipios" className="link-no-underline" onClick={() => handleItemClick('Municípios')}>
-          <SidebarItem Icon={FaHome} Text="Municípios" selected={selectedItem === 'Municípios'} />
+          <SidebarItem Icon={FaHome} Text="Home" selected={selectedItem === 'Municípios'} />
         </Link>
         <Link to="/coracao-de-maria" className="link-no-underline" onClick={() => handleItemClick('Coração de Maria')}>
-          <SidebarItem Icon={FaChartBar} Text="Coração de Maria" selected={selectedItem === 'Coração de Maria'} />
+          <SidebarItem Icon={FaHeart} Text="Coração de Maria" selected={selectedItem === 'Coração de Maria'} />
         </Link>
         <Link to="/conceicao-da-feira" className="link-no-underline" onClick={() => handleItemClick('Conceição da Feira')}>
-          <SidebarItem Icon={FaUserAlt} Text="Conceição da Feira" selected={selectedItem === 'Conceição da Feira'} />
+          <SidebarItem Icon={FaCross} Text="Conceição da Feira" selected={selectedItem === 'Conceição da Feira'} />
         </Link>
         <Link to="/taboao-da-serra" className="link-no-underline" onClick={() => handleItemClick('Taboão da Serra')}>
-          <SidebarItem Icon={FaEnvelope} Text="Taboão da Serra" selected={selectedItem === 'Taboão da Serra'} />
+          <SidebarItem Icon={FaFlag} Text="Taboão da Serra" selected={selectedItem === 'Taboão da Serra'} />
         </Link>
         <Link to="/feira-de-santana" className="link-no-underline" onClick={() => handleItemClick('Feira de Santana')}>
-          <SidebarItem Icon={FaRegCalendarAlt} Text="Feira de Santana" selected={selectedItem === 'Feira de Santana'} />
+          <SidebarItem Icon={FaChessRook} Text="Feira de Santana" selected={selectedItem === 'Feira de Santana'} />
         </Link>
         <Link to="/projetos" className="link-no-underline" onClick={() => handleItemClick('Projetos')}>
-          <SidebarItem Icon={FaIdCardAlt} Text="Projetos" selected={selectedItem === 'Projetos'} />
+          <SidebarItem Icon={FaCalendarDay} Text="Projetos" selected={selectedItem === 'Projetos'} />
         </Link>
       </Content>
     </Container>
